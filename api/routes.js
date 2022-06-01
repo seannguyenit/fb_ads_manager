1cd3c81f1e001e7821132b29b432001948bbb703
Sean Nguyen <thinhspgood@gmail.com>
2022-06-01 20:56:23 +0700

    fix top up card callback

--- api/routes.js
+++ api/routes.js
@@ -1,12 +1,12 @@
 'use strict';
 const multer = require('multer');
 
-const storage =  multer.diskStorage({ 
-    destination: function (req, file, next) {
-        next(null,`././static/img`);
-      },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname + '--'+ '.png');
+const storage = multer.diskStorage({
+  destination: function (req, file, next) {
+    next(null, `././static/img`);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + '--' + '.png');
   }
 });
 const upload = multer(
@@ -15,7 +15,7 @@ const upload = multer(
       next(null, true);
     }, limits: { fileSize: 15 * 1000000 }
   }
-  ).single('logo_img');
+).single('logo_img');
 module.exports = function (app) {
   let accCtrl = require('./controllers/user_controller');
   let menuCtrl = require('./controllers/menu_controller');
@@ -111,7 +111,7 @@ module.exports = function (app) {
     .get(menuCtrl.list_history_login);
 
   app.route('/api/logo')
-    .post(upload,menuCtrl.insert_logo);
+    .post(upload, menuCtrl.insert_logo);
   app.route('/api/menu_logo')
     .get(menuCtrl.get_logo);
   app.route('/api/menu_logo/:id')
@@ -198,6 +198,9 @@ module.exports = function (app) {
 
   app.route('/api/money_success')
     .post(moneyCtrl.successfully_topup);
+
+  app.route('/api/money_success_ticket')
+    .post(moneyCtrl.add_money_ticket);
 
   app.route('/api/list_withdraw/:id')
     .get(moneyCtrl.get_list_withdraw);
