31468ddae522e28cd1cfbd83793d142e705c379b
Sean Nguyen <thinhspgood@gmail.com>
2022-06-09 08:45:11 +0700

    fix email

--- api/controllers/user_controller.js
+++ api/controllers/user_controller.js
@@ -1,9 +1,10 @@
 'use strict'
 const util = require('util')
 const mysql = require('mysql')
-const nodeMailer = require('nodemailer')
+// const nodeMailer = require('nodemailer')
 const db = require('./../db')
 const path = require('path')
+const mail_config = require('./email_controller')
 
 module.exports = {
     get: (req, res) => {
@@ -168,7 +169,7 @@ module.exports = {
         let ok = 0;
         db.query(sql, [data.user, key_active, data.pass, data.ref], (err, response) => {
             if (err) throw err
-            sendMail(data.user, key_active).then(() => {
+            mail_config.sendMail(data.user, key_active).then(() => {
                 ok = 1
             }).catch((error) => {
                 ok = 0
@@ -183,7 +184,7 @@ module.exports = {
         let ok = 0;
         db.query(sql, [key_recovery, req.params.username], (err, response) => {
             if (err) throw err
-            sendMail_recovery(req.params.username, key_recovery).then(() => {
+            mail_config.sendMail_recovery(req.params.username, key_recovery).then(() => {
                 ok = 1
             }).catch((error) => {
                 ok = 0
@@ -517,68 +518,6 @@ module.exports = {
         })
     },
 
-}
-
-function sendMail(to, key_active) {
-    // Những thông tin dưới đây các bạn có thể ném nó vào biến môi trường env nhé.
-    // Vì để demo nên mình để các biến const ở đây.
-    const adminEmail = 'tool264sp@gmail.com'
-    const adminPassword = '#@*7LRmrfNZcn*@)'
-    // Mình sử dụng host của google - gmail
-    const mailHost = 'smtp.gmail.com'
-    // 587 là một cổng tiêu chuẩn và phổ biến trong giao thức SMTP
-    const mailPort = 587
-    // Khởi tạo một thằng transporter object sử dụng chuẩn giao thức truyền tải SMTP với các thông tin cấu hình ở trên.
-    const transporter = nodeMailer.createTransport({
-        host: mailHost,
-        port: mailPort,
-        secure: false, // nếu các bạn dùng port 465 (smtps) thì để true, còn lại hãy để false cho tất cả các port khác
-        auth: {
-            user: adminEmail,
-            pass: adminPassword
-        }
-    })
-
-    const options = {
-        from: adminEmail, // địa chỉ admin email bạn dùng để gửi
-        to: to, // địa chỉ gửi đến
-        subject: 'Đăng ký thành công tool264.com', // Tiêu đề của mail
-        html: `<h3>Chao mừng bạn đã đăng ký tool264.com</h3><div>Bấm vào link để kích hoạt tài khoản: </div><a target="_blank" href="https://tool264.com/account/activate/${key_active}">Kích hoạt</a>` // Phần nội dung mail mình sẽ dùng html thay vì thuần văn bản thông thường.
-    }
-
-    // hàm transporter.sendMail() này sẽ trả về cho chúng ta một Promise
-    return transporter.sendMail(options)
-}
-
-function sendMail_recovery(to, key_recovery) {
-    // Những thông tin dưới đây các bạn có thể ném nó vào biến môi trường env nhé.
-    // Vì để demo nên mình để các biến const ở đây.
-    const adminEmail = 'tool264sp@gmail.com'
-    const adminPassword = '#@*7LRmrfNZcn*@)'
-    // Mình sử dụng host của google - gmail
-    const mailHost = 'smtp.gmail.com'
-    // 587 là một cổng tiêu chuẩn và phổ biến trong giao thức SMTP
-    const mailPort = 587
-    // Khởi tạo một thằng transporter object sử dụng chuẩn giao thức truyền tải SMTP với các thông tin cấu hình ở trên.
-    const transporter = nodeMailer.createTransport({
-        host: mailHost,
-        port: mailPort,
-        secure: false, // nếu các bạn dùng port 465 (smtps) thì để true, còn lại hãy để false cho tất cả các port khác
-        auth: {
-            user: adminEmail,
-            pass: adminPassword
-        }
-    })
-
-    const options = {
-        from: adminEmail, // địa chỉ admin email bạn dùng để gửi
-        to: to, // địa chỉ gửi đến
-        subject: 'Đăng ký thành công tool264.com', // Tiêu đề của mail
-        html: `<h3>Chao mừng bạn đã đăng ký tool264.com</h3><div>Bấm vào link để Thay đổi mật khẩu: </div><a target="_blank" href="https://tool264.com/recovery_save?key=${key_recovery}">Kích hoạt</a>` // Phần nội dung mail mình sẽ dùng html thay vì thuần văn bản thông thường.
-    }
-
-    // hàm transporter.sendMail() này sẽ trả về cho chúng ta một Promise
-    return transporter.sendMail(options)
 }
 
 function uuidv4() {
