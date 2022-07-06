fb1e4603803e2019d3ac88a024388b4c12ca6a03
QuangTruong <quangtruong190523@gmail.com>
2022-07-06 09:54:50 +0700

    tiep tuc sua giao dien

--- static/js/p_guide.js
+++ static/js/p_guide.js
@@ -24,9 +24,9 @@ async function init_guide() {
                 var number2 =item.video.indexOf('watch?');
                 var link_video = item.video.slice(0, (number + 12)) + "embed/" + item.video.slice((number2 + 8));
                 document.getElementById("link_video").innerHTML +=`
-                                <div class="col-md-6 ">
-                                <div class="text_left pricing_benefit d-flex flex-column">
-                                    <div class="container text_center">
+                                <div class="col-md-6 pd0">
+                                <div class="text_left pricing_benefit pd0 d-flex flex-column">
+                                    <div class="container pd0 text_center">
                                     <iframe width="100%" height="300px" src="${link_video}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                     <p style="font-size: 24px !important;">${item.content_video}</p>
                                     </div>
